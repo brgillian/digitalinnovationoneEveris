4955d1fc528308eaf5aa50891bf0ade597142606
brgillian <brgillian@gmail.com>
2021-02-01 22:25:49 -0300

    GetMapping modificado para receber o CPF

--- c29AplicacoesWebSpringMVC/aula03/src/main/java/com/digitalinnovationone/aula03/controller/SoldadoController.java
+++ c29AplicacoesWebSpringMVC/aula03/src/main/java/com/digitalinnovationone/aula03/controller/SoldadoController.java
@@ -1,7 +1,12 @@
 package com.digitalinnovationone.aula03.controller;
 
+import com.digitalinnovationone.aula03.dto.Soldado;
+import com.digitalinnovationone.aula03.service.SoldadoService;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/soldado")
 public class SoldadoController {
     
-    @GetMapping
-    public String buscarSoldado(){
-        return "Olá, Soldado!";
+    private SoldadoService soldadoService;
+
+    public SoldadoController(SoldadoService soldadoService){
+        this.soldadoService = soldadoService;
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Soldado> buscarSoldado(@PathVariable() String cpf){
+       Soldado soldado = soldadoService.buscarSoldado(cpf);
+        return ResponseEntity.status(HttpStatus.OK).body(soldado);
     }
 }
