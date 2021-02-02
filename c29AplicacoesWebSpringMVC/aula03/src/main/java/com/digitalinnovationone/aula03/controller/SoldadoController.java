f43daab3ddf1b9397241ad1333632ee5385b73d7
brgillian <brgillian@gmail.com>
2021-02-01 22:38:00 -0300

    Adicionado um PostMapping para poder usar o Postman

--- c29AplicacoesWebSpringMVC/aula03/src/main/java/com/digitalinnovationone/aula03/controller/SoldadoController.java
+++ c29AplicacoesWebSpringMVC/aula03/src/main/java/com/digitalinnovationone/aula03/controller/SoldadoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class SoldadoController {
     public ResponseEntity<Soldado> buscarSoldado(@PathVariable() String cpf){
        Soldado soldado = soldadoService.buscarSoldado(cpf);
         return ResponseEntity.status(HttpStatus.OK).body(soldado);
+    }
+
+    @PostMapping
+    public ResponseEntity criarSoldado(@ResquestBody Soldado soldado){
+        return null;
     }
 }
