a10042584dc42ebcfcbd621e42dae425afe7c3a6
brgillian <brgillian@gmail.com>
2021-02-01 21:41:44 -0300

    Classe soldado criada

---
+++ c29AplicacoesWebSpringMVC/aula03/src/main/java/com/digitalinnovationone/aula03/dto/Soldado.java
@@ -0,0 +1,31 @@
+package com.digitalinnovationone.aula03.dto;
+
+public class Soldado {
+    private String nome;
+    private String raca;
+    private String arma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
